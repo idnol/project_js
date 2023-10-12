4b57881f1c3b30ff744abadc52e4b8e9f4845e26
cloudwipe <roma.isaev.2000@gmail.com>
2023-10-12 22:15:27 +0300

    update markup and css styles

--- src/js/book-markup.js
+++ src/js/book-markup.js
@@ -2,23 +2,22 @@ function renderBookCard(data) {
   return data
     .map(card => {
       return `  <li class="book-card">
-                  <a class='link' href="">
-                      <div class="book-action">
-                        <img
-                          class="book-image"
-                          src="${card.book_image}"
-                          alt="${card.title}"
-                          loading="lazy"
-                        />
-                        <div class="book-card-overlay">
-                          <p class="overlay-text">quick view</p>
-                        </div>
-                      </div>
-                      <div class="info">
-                        <p class="book-title">${card.title}</p>
-                        <p class="book-author">${card.author}</p>
-                      </div>
-                  </a>
+                  <div class="book-action">
+                    <img
+                      class="book-image"
+                      src="${card.book_image}"
+                      alt="${card.title}"
+                      loading="lazy"
+                    />
+                    <div class="book-card-overlay">
+                      <p class="overlay-text">quick view</p>
+                    </div>
+                    ;
+                  </div>
+                  <div class="info">
+                    <p class="book-title">${card.title}</p>
+                    <p class="book-author">${card.author}</p>
+                  </div>
                 </li>`;
     })
     .join('');
