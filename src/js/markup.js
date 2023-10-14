c57baadc8c556df38bb627e2a230f1a85b433cce
Dmytro Shkatula <idnol84@gmail.com>
2023-10-14 10:37:15 +0300

    update layout

--- src/js/markup.js
+++ src/js/markup.js
@@ -1,33 +1,19 @@
     import { renderMarkup } from './books';
- 
+
 function getCategoryMarkup(listResult) {
     const categoriesList = listResult.map(item => {
         return `<li class='categories-item js-category list'>${item.list_name}</li>`;
     }).join('');
 
     const markup = `
-        
+
         <ul class='categories'>
         <li class="categories-item js-category list">All categories</li>
             ${categoriesList}
         </ul>`;
-    
+
     return markup;
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 
 function createMarkup(apiResponse) {
@@ -36,16 +22,27 @@ function createMarkup(apiResponse) {
           const bookList = books
             .map(({ list_name, book_image, title, author }) => {
               return `
-            <li class="book" data-name="${list_name}">
-              <img class="book-img" src="${book_image}" alt="${title}" />
-              <h3 class="book-name">${title}</h3>
-              <p class="book-author">${author}</p>
-            </li>`;
-            })
-            .join('');
+                      <li class="book book-card" data-name="${list_name}">
+                        <div class="book-action">
+                          <img
+                            class="book-image"
+                            src="${book_image}"
+                            alt="${title}"
+                            loading="lazy"
+                          />
+                          <div class="book-card-overlay">
+                            <p class="overlay-text">quick view</p>
+                          </div>
+                        </div>
+                        <div class="info">
+                          <p class="book-title">${title}</p>
+                          <p class="book-author">${author}</p>
+                        </div>
+                      </li>`
+            }).join('');
           return `<ul class="category-books">
               <h2 class="category-name">${list_name}</h2>
-              <ul class="book-list">
+              <ul class="book-list list">
                   ${bookList}
               </ul>
               <button class="category-button" type="button" data-name="${list_name}">see more</button>
