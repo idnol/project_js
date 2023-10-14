18e2478de24e2886cd913591d17dee088f67c941
Dmytro Shkatula <idnol84@gmail.com>
2023-10-14 14:09:18 +0300

    update

--- src/js/handlers.js
+++ src/js/handlers.js
@@ -4,6 +4,12 @@ import { renderAllBooksInCategory } from './all-category-books.js';
 async function clickToCategory(e) {
   if (e.target.classList.contains('js-category-button') || e.target.classList.contains('js-category')) {
     await renderBooks(e);
+    if (e.target.classList.contains('js-category')) {
+      document.querySelectorAll('.js-category').forEach(item => {
+        item.classList.remove('active');
+      });
+      e.target.classList.add('active');
+    }
   }
 
   if (e.target.classList.contains('js-all')) {
