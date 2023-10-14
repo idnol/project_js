02853fcb7504c97fb48d7168b7b05dd8ac9d8007
TonyaLuts <131724958+TonyaLuts@users.noreply.github.com>
2023-10-14 15:08:12 +0100

    add current-page

--- src/js/header.js
+++ src/js/header.js
@@ -1,5 +1,14 @@
 // Mob-menu
-import { refs } from './refs'
+
+import { refs } from './refs';
+
+const refs = {
+  mobileMenu: document.querySelector('.js-mob-menu'),
+  openMenuBtn: document.querySelector('.js-open-menu'),
+  closeMenuBtn: document.querySelector('.js-close-menu'),
+
+  changeThemeBtn: document.querySelector('.switch-input'),
+};
 
 refs.openMenuBtn.addEventListener('click', toggleMenu);
 refs.closeMenuBtn.addEventListener('click', toggleMenu);
@@ -16,19 +25,34 @@ function toggleMenu() {
 
 export { toggleMenu };
 
+window.addEventListener('resize', matchMedia);
+
+function matchMedia(event) {
+  if (window.innerWidth > 768) {
+    // закрити меню
+    refs.mobileMenu.classList.remove('is-open');
+    refs.closeMenuBtn.classList.add('visually-hidden');
+    refs.openMenuBtn.classList.remove('visually-hidden');
+  }
+}
+
+export { refs, toggleMenu, matchMedia };
+
 // Current-page
 
-// let currentURL = window.location.href;
-// let links = document.getElementsByTagName('a');
+let currentURL = window.location.href;
+let links = document.getElementsByTagName('a');
+const itemList = document.querySelector('.menu-item');
 
-// function currentPage(e) {
-//   e.preventDefault();
+// console.log(currentURL);
+// console.log(links);
 
-// for (let link of links) {
-//   if (link.href === currentURL) {
-//     link.classList.add('current-page');
-//   } else {
-//     link.classList.remove('current-page');
-//   }
-// }
-// }
+for (let link of links) {
+  if (link.href === currentURL) {
+    link.classList.add('current-page');
+  } else {
+    link.classList.remove('current-page');
+  }
+}
+
+// dark-theme
--- src/main.js
+++ src/main.js
@@ -2,16 +2,18 @@ import { toggleMenu } from './js/header';
 import { refs } from './js/refs.js';
 import { getTopBooks, getCategoryList } from './js/api';
 import { bookSlider } from './js/slider';
-import {clickToCategory} from './js/handlers'
+import { clickToCategory } from './js/handlers';
 import { listenerEventsModal } from './js/modal';
 
-refs.bookCategories.addEventListener('click', clickToCategory)
-refs.sidebarCategories.addEventListener('click', clickToCategory)
+import { renderAllBooksInCategory } from './js/all-category-books';
+import { getBooksForCategory, getTopBooks } from './js/api';
+import { refs, toggleMenu, matchMedia } from './js/header';
+
+refs.bookCategories.addEventListener('click', clickToCategory);
+refs.sidebarCategories.addEventListener('click', clickToCategory);
 
 // toggleMenu();
 getTopBooks();
 bookSlider();
 getCategoryList();
 listenerEventsModal();
-
-
