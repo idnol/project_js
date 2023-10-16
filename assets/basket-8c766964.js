285d402302ddcba99dcd49772668eb9cb7ca74ae
idnol <30722271+idnol@users.noreply.github.com>
2023-10-16 20:59:10 +0000

    Deploying to gh-pages from @ idnol/project_js@216e3444a608e0d96a1239e031aeb2404b54aa31 🚀

--- assets/basket-8c766964.js
+++ assets/basket-8c766964.js
@@ -1,4 +1,4 @@
-import{r as i,s as w}from"./header-c7214fc8.js";function h(o){return o.length===0?`<li class="block-not-book">
+import{r as i,s as w}from"./header-04f901f6.js";function h(o){return o.length===0?`<li class="block-not-book">
             <h3 class="block-not-book_title">This page is empty, add some books and proceed to order.</h3>
             <picture class="block-not-book_img">
   <source
